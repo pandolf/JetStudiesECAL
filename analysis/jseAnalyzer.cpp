5f2ea05711dbf6953d374b909fb9c456545bcbeb
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-01-29 16:46:11 +0100

    saving response histos

--- analysis/jseAnalyzer.cpp
+++ analysis/jseAnalyzer.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1F.h"
 #include "TProfile.h"
 
+#include "../interface/jseCommon.h"
 
 
 
@@ -41,6 +45,8 @@ int main( int argc, char* argv[] ) {
   tree->SetBranchAddress("jet_mass", jet_mass );
   float jet_rawPt[999];
   tree->SetBranchAddress("jet_rawPt", jet_rawPt );
+  float jet_mcPt[999];
+  tree->SetBranchAddress("jet_mcPt", jet_mcPt );
 
   int jet_id[999];
   tree->SetBranchAddress("jet_id", jet_id );
@@ -69,7 +75,46 @@ int main( int argc, char* argv[] ) {
   TFile* outfile = TFile::Open( Form("%s/histoFiles/%s.root", prodName.c_str(), datasetName.c_str()), "RECREATE" );
   outfile->cd();
 
-  TProfile* hp_phEF_vs_eta = new TProfile( "phEF_vs_eta", "", 64, -3.2, 3.2 );
+  float etaMax = 3.2;
+  int nBins_eta = 64;
+
+  TProfile* hp_chEF_vs_eta    = new TProfile( "chEF_vs_eta"   , "", nBins_eta, -etaMax, etaMax );
+  TProfile* hp_nhEF_vs_eta    = new TProfile( "nhEF_vs_eta"   , "", nBins_eta, -etaMax, etaMax );
+  TProfile* hp_phEF_vs_eta    = new TProfile( "phEF_vs_eta"   , "", nBins_eta, -etaMax, etaMax );
+  TProfile* hp_elEF_vs_eta    = new TProfile( "elEF_vs_eta"   , "", nBins_eta, -etaMax, etaMax );
+  TProfile* hp_muEF_vs_eta    = new TProfile( "muEF_vs_eta"   , "", nBins_eta, -etaMax, etaMax );
+  TProfile* hp_hfhadEF_vs_eta = new TProfile( "hfhadEF_vs_eta", "", nBins_eta, -etaMax, etaMax );
+  TProfile* hp_hfemEF_vs_eta  = new TProfile( "hfemEF_vs_eta" , "", nBins_eta, -etaMax, etaMax );
+
+
+  std::vector<float> ptBins  = jseCommon::ptBins();
+  std::vector<float> etaBins = jseCommon::etaBins();
+
+  std::cout << "etaBins:" << std::endl;
+  for(unsigned i=0; i<etaBins.size(); ++i )
+    std::cout << etaBins[i] << std::endl;
+
+  std::map< std::string, TH1D* > map_resp;
+
+  for(unsigned iPt=0; iPt<ptBins.size()-1; ++iPt) {
+
+    //std::vector<TH1D*> vh1_resp;
+
+    for(unsigned iEta=0; iEta<etaBins.size()-1; ++iEta) {
+
+      std::string thisHistoName( jseCommon::getPtEtaHistoName( "resp", iPt, iEta ) ); 
+      //std::string thisHistoName( jseCommon::getPtEtaHistoName( "resp", ptBins[iPt], ptBins[iPt+1], etaBins[iEta], etaBins[iEta+1] ) ); 
+      TH1D* h1_thisResponse = new TH1D( thisHistoName.c_str(), "", 100, 0.5, 2. );
+      
+      //vh1_resp.push_back(h1_thisResponse);
+      map_resp[thisHistoName] = h1_thisResponse;
+
+    } // for eta
+
+    //h1_response.push_back(vh1);
+
+  } // for pt
+
 
 
   int nentries = tree->GetEntries();
@@ -83,17 +128,37 @@ int main( int argc, char* argv[] ) {
 
     for( unsigned ijet=0; ijet<njet; ++ijet ) {
 
-      hp_phEF_vs_eta->Fill( jet_eta[ijet], jet_phEF[ijet] );
+      hp_chEF_vs_eta   ->Fill( jet_eta[ijet], jet_chEF[ijet]    );
+      hp_nhEF_vs_eta   ->Fill( jet_eta[ijet], jet_nhEF[ijet]    );
+      hp_phEF_vs_eta   ->Fill( jet_eta[ijet], jet_phEF[ijet]    );
+      hp_elEF_vs_eta   ->Fill( jet_eta[ijet], jet_elEF[ijet]    );
+      hp_muEF_vs_eta   ->Fill( jet_eta[ijet], jet_muEF[ijet]    );
+      hp_hfhadEF_vs_eta->Fill( jet_eta[ijet], jet_hfhadEF[ijet] );
+      hp_hfemEF_vs_eta ->Fill( jet_eta[ijet], jet_hfemEF[ijet]  );
+
+
+      std::string histoName = jseCommon::findHistoName( "resp", jet_pt[ijet], jet_eta[ijet] );
+      if( map_resp.find(histoName) != map_resp.end() )
+        map_resp[histoName]->Fill( jet_pt[ijet]/jet_mcPt[ijet] );
+
 
     }  // for jets
-
 
   } // for entries
 
 
   outfile->cd();
 
-  hp_phEF_vs_eta->Write();
+  hp_chEF_vs_eta   ->Write();
+  hp_nhEF_vs_eta   ->Write();
+  hp_phEF_vs_eta   ->Write();
+  hp_elEF_vs_eta   ->Write();
+  hp_muEF_vs_eta   ->Write();
+  hp_hfhadEF_vs_eta->Write();
+  hp_hfemEF_vs_eta ->Write();
+
+  for( std::map<std::string,TH1D*>::const_iterator iresp=map_resp.begin(); iresp!=map_resp.end(); ++iresp )
+    iresp->second->Write();
 
   outfile->Close();
 
