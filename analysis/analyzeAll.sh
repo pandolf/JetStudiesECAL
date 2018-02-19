20fe7228e9dd71b5302dd3f9eb6556b7e49c2d08
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-02-19 15:53:44 +0100

    10X version

--- analysis/analyzeAll.sh
+++ analysis/analyzeAll.sh
@@ -2,11 +2,17 @@ if [ $# -lt 1 ]
 then
   echo "Please provide production name and pass it as first argument: ./analyzeAll [productionName]"
 else
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF_noPU
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SRON
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF_noPU
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON_noPU
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF_noPU
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SRON
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF_noPU
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON_noPU
+  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_30fb
+  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_0fb
+  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_60fb
+  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_30fb
+  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_0fb
+  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_60fb
 fi
