c4c6157bb8e491dfdb7893fa1c4387e761cc9f38
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-02-19 15:54:05 +0100

    10X version

--- analysis/mergeAllTrees.sh
+++ analysis/mergeAllTrees.sh
@@ -2,11 +2,17 @@ if [ $# -lt 1 ]
 then
   echo "Please provide production name and pass it as first argument: ./mergeAllTrees.sh [productionName]"
 else
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF_noPU
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SRON
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF_noPU
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON
-  ./jseMergeTrees $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON_noPU
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SROFF_noPU
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_ECALnoiseOFF_SRON
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF_noPU
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON
+#  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON_noPU
+  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13_noPU_30fb
+  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13_noPU_0fb
+  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13_noPU_60fb
+  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13_30fb
+  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13_0fb
+  ./jseMergeTrees 10X $1 RelValQCD_FlatPt_15_3000HS_13_60fb
 fi
