77576f46b096a7d230169b88fc16bb43d6ab9dfc
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-03-13 15:16:46 +0100

    added PFthA,B,C samples from mar9

--- analysis/analyzeAll.sh
+++ analysis/analyzeAll.sh
@@ -9,10 +9,16 @@ else
   #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SROFF_noPU
   #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON
   #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13UP17_GRv2_SRON_noPU
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_30fb
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_0fb
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_60fb
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_30fb
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_0fb
-  ./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_60fb
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_30fb
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_0fb
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_noPU_60fb
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_30fb
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_0fb
+  #./jseAnalyzer $1 RelValQCD_FlatPt_15_3000HS_13_60fb
+  ./jseAnalyzer $1  RelValQCD_FlatPt_15_3000_PFthA_noPU
+  ./jseAnalyzer $1  RelValQCD_FlatPt_15_3000_PFthB_noPU
+  ./jseAnalyzer $1  RelValQCD_FlatPt_15_3000_PFthC_noPU
+  #./jseAnalyzer $1 RelValZEE_rel2007_GTv4
+  #./jseAnalyzer $1 RelValZEE_rel2008_GTv4
+  #./jseAnalyzer $1 RelValZEE_rel2008_GTv7
 fi
