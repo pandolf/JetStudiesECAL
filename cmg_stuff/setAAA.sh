dcee2f79fb589d1f2a7194c962e4163a53dbdcdd
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-12-14 11:52:45 +0100

    works in 94X

--- cmg_stuff/setAAA.sh
+++ cmg_stuff/setAAA.sh
@@ -1,5 +1,5 @@
 voms-proxy-init --voms cms --valid 168:00
 cp /tmp/x509up_u27697 `pwd`/X509_USER_PROXY
-export X509_USER_PROXY=`pwd`/X509_USER_PROXY
+setenv X509_USER_PROXY `pwd`/X509_USER_PROXY
 echo "X509_USER_PROXY: "
 echo $X509_USER_PROXY
